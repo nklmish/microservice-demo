f28e31aa9199d19cbbafa4277558e53a29cb6088
nklmish <nkl.msr@gmail.com>
2015-08-30 17:36:15 +0200

    comment service on java 8

---
+++ comment-service/src/main/java/com/nklmish/cs/comments/Application.java
@@ -0,0 +1,14 @@
+package com.nklmish.cs.comments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
