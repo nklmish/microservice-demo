8f78c8ea4c9af7aa84ab5df5c56242ae11c97871
nklmish <coding.aurora@gmail.com>
2015-10-05 20:47:13 +0200

    rabbitMq connection factory bean

--- catalog-service/src/main/groovy/com/nklmish/cs/catalogs/Application.groovy
+++ catalog-service/src/main/groovy/com/nklmish/cs/catalogs/Application.groovy
@@ -1,14 +1,26 @@
 package com.nklmish.cs.catalogs
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 class Application {
+
+    @Value('${app.rabbitmq.host}')
+    String host
+
+    @Bean
+    public ConnectionFactory createRabbitMqConnectionFactory() {
+       return new CachingConnectionFactory(host)
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application, args)
