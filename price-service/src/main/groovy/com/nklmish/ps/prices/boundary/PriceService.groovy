0ef82a862f53db30be52f44c38e079a807a3fb12
nklmish <coding.aurora@gmail.com>
2015-10-08 18:31:49 +0200

    random price generation + price metrics

--- price-service/src/main/groovy/com/nklmish/ps/prices/boundary/PriceService.groovy
+++ price-service/src/main/groovy/com/nklmish/ps/prices/boundary/PriceService.groovy
@@ -1,10 +1,15 @@
 package com.nklmish.ps.prices.boundary
 
+import com.codahale.metrics.Counter
+import com.codahale.metrics.MetricRegistry
 import com.nklmish.ps.prices.model.Price
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+import javax.annotation.PostConstruct
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 
@@ -13,9 +18,33 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 @Slf4j
 class PriceService {
 
+    public static final BigDecimal PROFIT_THRESHOLD = BigDecimal.valueOf(1200.0)
+
+    @Autowired
+    MetricRegistry metrics
+
+    private  Counter expensiveProducts
+
+    private int randomness = new Random().nextInt((100 - 10) + 1) + 10
+
     @RequestMapping(value = "/{productId}", produces = APPLICATION_JSON_VALUE)
     Price findComments(@PathVariable Integer productId) {
         log.debug("retrieving price for product {}", productId)
-        return new Price(1, productId, 1000.20)
+        BigDecimal calculatedPrice = BigDecimal.valueOf(productId).multiply(randomness)
+
+        reportIfProfitable(calculatedPrice)
+
+        return new Price(1, productId, calculatedPrice)
+    }
+
+    private void reportIfProfitable(BigDecimal calculatedPrice) {
+        if (calculatedPrice.compareTo(PROFIT_THRESHOLD) > 0) {
+            expensiveProducts.inc()
+        }
+    }
+
+    @PostConstruct
+    public void registerCounter() {
+        expensiveProducts = metrics.counter("expensive-products")
     }
 }
